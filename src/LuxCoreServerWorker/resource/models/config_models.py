db249bb08c3296d94822aad5c28c4dd0e6fd2804
Daniel Moreira Guimarães <danielmguimaraes@poli.ufrj.br>
2019-07-23 00:07:30 -0300

    New way to define the input payload schema

--- src/LuxCoreServerWorker/resource/models/config_models.py
+++ src/LuxCoreServerWorker/resource/models/config_models.py
@@ -1,9 +1,32 @@
+from pathlib import Path
+import csv
+import json
+
 from flask_restplus import fields
 
 from ...server.instance import server
 
 api = server.api
+api.models
 config_post_model = api.model('Render configuration model',
  {'renderengine.type': fields.String,
  'int': fields.Integer,
  'listInts': fields.List(fields.Integer)})
+
+address = api.schema_model('Address', {
+    'properties': {
+        'road': {
+            'type': 'string'
+        },
+    },
+    'type': 'object'
+})
+
+basepath = Path(__file__).parent
+filepath = (basepath / "config_models_schema.json").resolve()
+
+with open(str(filepath),'r') as f:
+    schema = f.read()
+schema_json = json.loads(schema)
+config_post_model = address = api.schema_model('Config files', schema_json)
+
